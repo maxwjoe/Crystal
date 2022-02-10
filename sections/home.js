4259f472a15f3c9f7f26769234f2a6f7fc46eb3a
Joel <joel.r.maxwell@gmail.com>
2022-02-10 11:07:39 +1100

    learn more button now works

---
+++ sections/home.js
@@ -0,0 +1,17 @@
+const learn_more_button = document.querySelector('.home-call-to-action button');
+
+let about_section = undefined;
+
+sections.forEach(section => {
+    if (section.getAttribute('id') === 'about') {
+        about_section = section;
+    }
+})
+
+learn_more_button.addEventListener('click', function () {
+    const view = getViewport();
+
+    const sectionTop = about_section.offsetTop;
+
+    window.scrollTo(0,sectionTop - 0.1 * view[1]);
+})
